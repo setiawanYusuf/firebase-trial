1693f0f25e4ed7b3ea3d73064eafea14c87acbde
yusufSetiawan <yusuf.setiawan@kly.id>
2018-10-30 09:55:00 +0700

    Update master in office's PC

---
+++ firebaseAuth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyDa-5yYAeaeo1OMLKna8KnkbHPd8Nvflu4",
+    authDomain: "trialnodejsfirebase.firebaseapp.com",
+    databaseURL: "https://trialnodejsfirebase.firebaseio.com",
+    projectId: "trialnodejsfirebase",
+    storageBucket: "trialnodejsfirebase.appspot.com",
+    messagingSenderId: "983835165150"
+}
